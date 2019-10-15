d1b13331c42e2995f6ae44c0c9f6908b3bf4e1a9
Por <pdumrhi@sertiscorp.com>
2019-10-15 15:31:25 +0700

    update responsive left menu behav

--- src/components/Content/styledComponents.js
+++ src/components/Content/styledComponents.js
@@ -5,6 +5,7 @@ export const ContainerWrapper = styled.div`
   max-width: 1280px;
   padding: 4rem;
   flex:1;
+  overflow: auto;
   /* height: 100%;
   box-sizing: border-box; */
   .card-wrapper {
--- src/containers/LeftMenu/index.js
+++ src/containers/LeftMenu/index.js
@@ -1,24 +1,40 @@
 import React from 'react'
 import { AppWrapper } from '../TopMenu/styledComponents'
 import Content from '../../components/Content'
-import { LeftMenuWrapper, TopWrapperMenu } from './styledComponents'
+import { LeftMenuWrapper } from './styledComponents'
 class LeftMenu extends React.Component {
+  constructor(props) {
+    super(props)
+    this.openMenu = this.openMenu.bind(this)
+    this.closeMenu = this.closeMenu.bind(this)
+    this.state = {
+      menuSmallScreen: ''
+    }
+  }
+
+  openMenu() {
+    this.setState({menuSmallScreen: 'open'})
+  }
+
+  closeMenu() {
+    this.setState({menuSmallScreen: ''})
+  }
+
   render() {
     return (
       <AppWrapper direction={'row'} display={'flex'}>
-        <TopWrapperMenu>
-          <div>A</div>
-          <div>B</div>
-          <div>C</div>
-          <div>D</div>
-          <div>E</div>
-        </TopWrapperMenu>
         <LeftMenuWrapper>
-          <div>A</div>
-          <div>B</div>
-          <div>C</div>
-          <div>D</div>
-          <div>E</div>
+          <div className="start-nav">A</div>
+          <div className={`middle-nav ${this.state.menuSmallScreen}`}>
+            <div className="menu-item">B</div>
+            <div className="menu-item">C</div>
+            <div className="menu-item">D</div>
+            <div className="menu-item">E</div>
+            <div className="menu-item">F</div>
+          </div>
+          <div className="end-nav">
+            <div className="menu-toggle" onClick={this.openMenu} onBlur={this.closeMenu} tabIndex={0}>G</div>
+          </div>
         </LeftMenuWrapper>
         <Content />
       </AppWrapper>
@@ -26,3 +42,20 @@ class LeftMenu extends React.Component {
   }
 }
 export default LeftMenu
+//
+// const nav = <TopNavbar>
+//   <div className="start-nav">A</div>
+//   <div className="middle-nav">
+//     <div className={`menu-wrapper ${this.state.menuState}`}>
+//       <div className="menu-item">B</div>
+//       <div className="menu-item">C</div>
+//       <div className="menu-item">D</div>
+//       <div className="menu-item">E</div>
+//       <div className="menu-item">F</div>
+//     </div>
+//   </div>
+//   <div className="end-nav">
+//     <div className="action">G</div>
+//     <div className="action responsive" tabIndex={0} onClick={this.openMenu} onBlur={this.closeMenu}>H</div>
+//   </div>
+// </TopNavbar>
